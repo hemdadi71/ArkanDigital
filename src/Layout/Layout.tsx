3edbceeedcc76866afc8a1c8bdde04136ab4ccee
Hosein Emdadi <hemdadi71@gmail.com>
2023-06-04 14:29:35 +0430

    tymes an schema folders fixed

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { LayoutProps } from '../../Types/types'
+import { LayoutProps } from '../../Interfaces'
 import Header from '@/Components/Header/Header'
 import Footer from '@/Components/Footer/Footer'
 
 function Layout({ children }: LayoutProps) {
   return (
     <>
-    
-        <Header />
-        {children}
-        <Footer />
+      <Header />
+      {children}
+      <Footer />
     </>
   )
 }
